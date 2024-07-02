cc9dd141ead386f31c745ca0c9f7cb2143534e2b
Phat <nvtpk19hutech@gmail.com>
2024-07-02 16:27:33 +0700

    123

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -34,23 +34,25 @@ const projects: Projects[] = [
 export default function Projects() {
     return (
         <>
-            <div className="grid grid-cols-1 gap-4 xl:grid-cols-4 mx-auto container">
-               {projects.map((item) => (
-                <div
-                    key={item.id}
-                    className="animate_top group relative rounded-lg border border-stroke bg-white p-8 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none"
-                >
-                    <div className="absolute -right-3.5 top-7.5 -rotate-90 rounded-bl-full rounded-tl-full bg-primary px-4.5 py-1.5 text-metatitle font-medium uppercase text-white">
-                        Phổ Biến
-                    </div>
-                    <h3 className="mb-7.5 text-2xl font-bold text-black dark:text-white">
-                        {item.label} Platform
-                    </h3>
-                    <div className="border-t border-stroke pb-12.5 pt-9 dark:border-strokedark">
-                        <img src={item.image} alt={`Brand ${item.id}`} />
-                    </div>
+            <div className=' mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+                <div className="grid grid-cols-1 gap-4 xl:grid-cols-4">
+                    {projects.map((item) => (
+                        <div
+                            key={item.id}
+                            className="animate_top group relative rounded-lg border border-stroke bg-white p-8 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none"
+                        >
+                            <div className="absolute -right-3.5 top-7.5 -rotate-90 rounded-bl-full rounded-tl-full bg-primary px-4.5 py-1.5 text-metatitle font-medium uppercase text-white">
+                                Phổ Biến
+                            </div>
+                            <h3 className="mb-7.5 text-2xl font-bold text-black dark:text-white">
+                                {item.label}
+                            </h3>
+                            <div className="border-t border-stroke pb-12.5 pt-9 dark:border-strokedark">
+                                <img src={item.image} alt={`Brand ${item.id}`} />
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
             </div>
         </>
     )
