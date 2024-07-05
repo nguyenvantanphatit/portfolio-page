646de5a20da8ed11003973d61e393151af198fe4
Tan Phat <nguyenvantanphat.it@gmail.com>
2024-07-05 23:32:43 +0700

    123

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,14 @@ export const metadata: Metadata = {
   description: "Tấn Phát Software Engineer",
   keywords: "Tấn Phát Software Engineer",
   authors: [{ name: "Tấn Phát Team" }],
+  metadataBase: new URL('https://echomedi-page.vercel.app'),
   openGraph: {
     title: "Tấn Phát",
     description: "Tấn Phát Software Engineer",
     url: "https://echomedi-page.vercel.app",
+    siteName: 'Tấn Phát Software Engineer',
     type: "website",
+    locale: 'en_US',
     images: [
       {
         url: 'https://res.cloudinary.com/dq49gyq1g/image/upload/v1720057490/aqfvydnqjwbzhoniyio9.png',
@@ -22,6 +25,28 @@ export const metadata: Metadata = {
         alt: 'Tấn Phát Software Engineer'
       }
     ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Tấn Phát Software Engineer',
+    description: 'Tấn Phát Software Engineer',
+    siteId: '1467726470533754880',
+    creator: '@your_twitter_handle',
+    creatorId: '1467726470533754880',
+    images: ['https://res.cloudinary.com/dq49gyq1g/image/upload/v1720057490/aqfvydnqjwbzhoniyio9.png'], 
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
 };
 
