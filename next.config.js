806124ebb8b41c7d85b2272b980160385594ad1a
Phat <nvtpk19hutech@gmail.com>
2024-07-05 16:13:23 +0700

    123

--- next.config.js
+++ next.config.js
@@ -5,8 +5,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
     domains: ["d3e4m6b6rxmux9.cloudfront.net", "i.imgur.com"],
-    },
-   
+  },
+  env: {
+    metadata: {
+      metadataBase: "https://echomedi-page.vercel.app"
+    }
+  }
 };
 
 module.exports = nextConfig;
