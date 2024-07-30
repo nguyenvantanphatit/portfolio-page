e529f749d4367fb3d5117ff2b93aaa2f3060de38
Phat <nvtpk19hutech@gmail.com>
2024-07-30 09:21:19 +0700

    123

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,21 @@
+"use client"
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollPosition(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <section className="relative md:pt-40 pt-28 pb-10 bg-slate-50">
             <img src="/images/banner/banner.jpg" alt="cover-image" className="w-full absolute top-0 left-0 z-0 md:h-1/2 h-50" />
@@ -17,62 +32,28 @@ const Header = () => {
                         <a href="/#contact"
                             className="py-3.5 px-5 flex rounded-full bg-indigo-600 items-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-700">
                             <span className="px-2 font-semibold text-base leading-7 text-white">Message</span>
-                            {/* <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M11.3011 8.69881L8.17808 11.8219M8.62402 12.5906L8.79264 12.8819C10.3882 15.6378 11.1859 17.0157 12.2575 16.9066C13.3291 16.7974 13.8326 15.2869 14.8397 12.2658L16.2842 7.93214C17.2041 5.17249 17.6641 3.79266 16.9357 3.0643C16.2073 2.33594 14.8275 2.79588 12.0679 3.71577L7.73416 5.16033C4.71311 6.16735 3.20259 6.67086 3.09342 7.74246C2.98425 8.81406 4.36221 9.61183 7.11813 11.2074L7.40938 11.376C7.79182 11.5974 7.98303 11.7081 8.13747 11.8625C8.29191 12.017 8.40261 12.2082 8.62402 12.5906Z"
                                     stroke="white" stroke-width="1.6" stroke-linecap="round" />
-                            </svg> */}
-                          
-                           <svg className="circle-svg" viewBox="0 0 500 500">
-                                        <defs>
-                                            <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle_top">
-                                                <animateTransform attributeName="transform" begin="0s" dur="20s" type="rotate" from="0 250 250" to="360 250 250" repeatCount="indefinite" />
-                                            </path>
-                                        </defs>
-                                        <text className="circle-text" dy="70" textLength="1220">
-                                            <textPath xlinkHref="#textcircle_top">
-                                                Front-end Developer
-                                            </textPath>
-                                        </text>
-                                    </svg>
+                            </svg>
                         </a>
                     </div>
                 </div>
                 <div className="flex max-sm:flex-wrap max-sm:justify-between items-center">
                     <div className="md:relative">
-                        <div className='flex items-center justify-center min-h-screen from-gray-100 via-gray-300 to-gray-500 bg-gradient-to-br'>
-                            <div className="relative w-full h-screen">
-                                <div className="absolute-center">
-                                    <svg className="circle-svg custom-spin-slow" viewBox="0 0 500 500">
-                                        <defs>
-                                            <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle_top">
-                                                <animateTransform attributeName="transform" begin="0s" dur="20s" type="rotate" from="0 250 250" to="360 250 250" repeatCount="indefinite" />
-                                            </path>
-                                        </defs>
-                                        <text className="circle-text" dy="70" textLength="1220">
-                                            <textPath xlinkHref="#textcircle_top">
-                                                Front-end Developer
-                                            </textPath>
-                                        </text>
-                                    </svg>
-                                </div>
-                                {/* <div className="absolute-center">
-                                    <div className="showreels-div">
-                                        <video className="showreels-video" loop autoPlay muted>
-                                            <source src='https://github.com/ahampriyanshu/gfg/raw/main/media/showreel.mp4' type="video/mp4" />
-                                            <p>Please Update Your Browser</p>
-                                        </video>
-                                        <Image
-                                            alt="play"
-                                            className="showreels-btn"
-                                            src='https://github.com/ahampriyanshu/gfg/raw/main/media/play.png'
-                                            width={50}
-                                            height={50}
-                                        />
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div>
+                        <svg style={{ top: `calc(5% + ${scrollPosition}px)` }} className="circle-svg custom-spin-slow circle-svg-center" viewBox="0 0 500 500">
+                            <defs>
+                                <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle_top">
+                                    <animateTransform attributeName="transform" begin="0s" dur="20s" type="rotate" from="0 250 250" to="360 250 250" repeatCount="indefinite" />
+                                </path>
+                            </defs>
+                            <text className="circle-text" dy="70" textLength="1220">
+                                <textPath xlinkHref="#textcircle_top">
+                                    Front-end Developer Front-end Developer Front-end Developer
+                                </textPath>
+                            </text>
+                        </svg>
                     </div>
                     <div className="md:relative">
                         <Image src="/images/banner/Logo.svg" width={150} height={150} className="custom-spin-slow hidden md:block" alt="Logo" />
